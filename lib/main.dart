360bdea5e58590edf00ab2df171bb27a4f7cd9c7
mohamed <mazn18675@gamil.com>
2024-09-14 20:06:07 +0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -36,120 +35,124 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
-        body: Center(
-            child: Column(
-          children: [
-            Image.asset(
-              'images/p2.png',
-              scale: 2.4,
-            ),
-            SizedBox(height: sizee.height * 0.075),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Team A",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 44,
-                            fontWeight: FontWeight.w300,
-                            fontFamily: "Bangers"),
-                      ),
-                      Text(
-                        "$countA",
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 70,
-                            fontWeight: FontWeight.w300,
-                            fontFamily: "Bangers"),
-                      ),
-                      SizedBox(height: sizee.height * 0.23),
-                      TextButton(
-                        onPressed: () {
-                          setState(() {
-                            countA++;
-                          });
-                        },
-                        child: const Text(
-                          "Add 1 Point",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
-                              fontWeight: FontWeight.w300,
-                              fontFamily: "Bangers"),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Team B",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 44,
-                            fontWeight: FontWeight.w300,
-                            fontFamily: "Bangers"),
-                      ),
-                      Text(
-                        "$countB",
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 70,
-                            fontWeight: FontWeight.w300,
-                            fontFamily: "Bangers"),
-                      ),
-                      SizedBox(
-                        height: sizee.height * 0.23,
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          setState(() {
-                            countB++;
-                          });
-                        },
-                        child: const Text(
-                          "Add 1 Point",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
-                              fontWeight: FontWeight.w300,
-                              fontFamily: "Bangers"),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: sizee.height * 0.04,
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  countA = 0;
-                  countB = 0;
-                });
-              },
-              child: const Text(
-                "Reset",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                    fontWeight: FontWeight.w300,
-                    fontFamily: "Bangers"),
+        body: SizedBox(
+          height: sizee.height,
+          child: Center(
+              child: Column(
+            children: [
+              Image.asset(
+                'images/p2.png',
+                scale: 2.4,
               ),
-            ),
-          ],
-        )),
+              SizedBox(height: sizee.height * 0.075),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Team A",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 44,
+                              fontWeight: FontWeight.w300,
+                              fontFamily: "Bangers"),
+                        ),
+                        Text(
+                          "$countA",
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 70,
+                              fontWeight: FontWeight.w300,
+                              fontFamily: "Bangers"),
+                        ),
+                        SizedBox(height: sizee.height * 0.23),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              countA++;
+                            });
+                          },
+                          child: const Text(
+                            "Add 1 Point",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.w300,
+                                fontFamily: "Bangers"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Team B",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 44,
+                              fontWeight: FontWeight.w300,
+                              fontFamily: "Bangers"),
+                        ),
+                        Text(
+                          "$countB",
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 70,
+                              fontWeight: FontWeight.w300,
+                              fontFamily: "Bangers"),
+                        ),
+                        SizedBox(
+                          height: sizee.height * 0.23,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              countB++;
+                            });
+                          },
+                          child: const Text(
+                            "Add 1 Point",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.w300,
+                                fontFamily: "Bangers"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: sizee.height * 0.04,
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    countA = 0;
+                    countB = 0;
+                  });
+                },
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w300,
+                      fontFamily: "Bangers"),
+                ),
+              ),
+              Spacer()
+            ],
+          )),
+        ),
       ),
     );
   }
